bcc05fa8db40bede87b60bdfa555847701765a14
VBTX <jv.svedaite@gmail.com>
2019-03-26 16:37:22 -0500

    add index, create routes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,12 @@
 class ListsController < ApplicationController
 	def index
+		@list = List.new
 		@lists = List.all
+	end
+	def show
+		@list = List.find(params[:id])
+	end
+	def create
+		
 	end
 end
